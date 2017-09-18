d24df123c97aba6609dc74503b7ad48697c95fc8
Chris Nunez <cenunez47@gmail.com>
2017-09-18 03:13:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECONDS(S)!"
+    number -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+  binding.pry
 end
